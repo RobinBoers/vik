b605d1ccc1971889ba64b77e8804c80e2bbe5c74
Robin <robindev2019@outlook.com>
2025-04-27 15:43:17 +0200

    Implement `Repo.delete_by!/2`

--- lib/vik/repo.ex
+++ lib/vik/repo.ex
@@ -2,4 +2,16 @@ defmodule Vik.Repo do
   use Ecto.Repo,
     otp_app: :vik,
     adapter: Ecto.Adapters.Postgres
+
+  @doc """
+  Deletes a struct using an arbitrary query.
+
+  See `get_by!/2` and `delete!/2`.
+  """
+  @spec delete_by!(Ecto.Queryable.t(), Keyword.t() | map()) :: Ecto.Schema.t()
+  def delete_by!(queryable, clauses) do
+    queryable
+    |> get_by!(clauses)
+    |> delete!()
+  end
 end
