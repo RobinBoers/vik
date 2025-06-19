e07103851a0d5f840d760a2d8dca7f042e28461e
Robin <robindev2019@outlook.com>
2025-06-19 19:12:33 +0200

    Who needs tests anyway

--- test/vik_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule VikWeb.PageControllerTest do
-  use VikWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
