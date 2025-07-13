68fd9d9cb95af303c994143310d2ad9dd4e5a5bd
Robin <robindev2019@outlook.com>
2025-07-13 13:41:39 +0200

    Bump minimum Elixir version to 1.18

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Vik.MixProject do
       name: "Vik",
       app: :vik,
       version: "0.0.1-rc1",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
